edc14af93e481f394552769d9f1b7f77965e5e6f
Cristina <cris.barbagallo@gmail.com>
2018-10-23 13:43:41 +0200

    Added includes

---
+++ includes/products.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php
+
+$products = array (
+        'A' => array (
+                'name' => 'Plant',
+                'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                'price' => 100,
+                'image' => '/images/image_two.png'
+        ),
+        'B' => array (
+                'name' => 'Orange Flowers',
+                'desc' => 'Ipsum dolor sit amet. Amet adipisicing numquam aspernatur!',
+                'price' => 59,
+                'image' => '/images/image_four.png' 
+
+        ),
+        'C' => array (
+                'name' => 'Tree',
+                'desc' => 'Consectetur adipisicing elit. Amet numquam aspernatur!',
+                'price' => 250,
+                'image' => '/images/image_one.png'
+        ),
+        'D' => array (
+                'name' => 'Red Flowers',
+                'desc' => 'Consectetur adipisicing elit. Amet numquam aspernatur!',
+                'price' => 79,
+                'image' => '/images/image_six.png'
+        ),
+        'E' => array (
+                'name' => 'Cactus',
+                'desc' => 'Consectetur adipisicing elit. Amet numquam aspernatur!',
+                'price' => 190,
+                'image' => '/images/image_three.png'
+        ),
+        'F' => array (
+                'name' => 'Tulips',
+                'desc' => 'Consectetur adipisicing elit. Amet numquam aspernatur!',
+                'price' => 280,
+                'image' => '/images/image_five.png'
+        ) 
+);
+
+?>
+
+
+</body>
+</html>
+
+
